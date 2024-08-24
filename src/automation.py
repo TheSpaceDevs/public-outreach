7bc0fc560a9d409cd468e84d64e53237125d1735
Arnaud Muller <arnaud.muller1308@gmail.com>
2024-08-24 18:35:21 +0200

    wip

--- src/automation.py
+++ src/automation.py
@@ -1,4 +1,4 @@
-from src.tsd_users_plotly import generate_tsd_reach_plot, generate_tsd_user_types_plot
+from tsd_users_plotly import generate_tsd_reach_plot, generate_tsd_user_types_plot
 
 if __name__ == "__main__":
     generate_tsd_reach_plot()
